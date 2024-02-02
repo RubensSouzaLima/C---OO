75a4a1c85f9c6ff743939b554127a95c1e59b716
Rubens Lima <rubens.souzalima@hotmail.com>
2024-02-01 22:47:58 -0300

    C# Atividades de programacao Orientada a Objetos

---
+++ CourseSB/CourseSB/Enitites/Comment.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace CourseSB.Enitites
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() 
+        { 
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+    }
+}
---
+++ HerancaAndPolimorfismoEx2/HerancaAndPolimorfismoEx2/Entities/ImportedProduct.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Globalization;
+
+namespace HerancaAndPolimorfismoEx2.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() 
+        {
+        
+        }
+
+        public ImportedProduct(string name, double price, double customFee) 
+            : base(name, price)
+        {
+            CustomsFee = customFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+            + " $ "
+            + Price
+            + "Customs Fee: $ "
+            + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+            + ")";
+        }
+    }
+}
